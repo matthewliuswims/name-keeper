9d232801255ab00a6cfe2ebe8200b5e393afb9e4
matthewliuswims <matthew@matthewliu.net>
2019-03-10 14:28:10 -0500

    some minor changes to the about screen

--- src/assets/screens/help/AboutScreen.js
+++ src/assets/screens/help/AboutScreen.js
@@ -22,7 +22,7 @@ export default class About extends Component {
           <Text style={styles.AnswerText}>We&apos;ve all been there. You know that person&apos;s face. You remember his horn-rimmed glasses, fiery-red hair and shy demeanor. BUT: you just cannot remember his name.
           </Text>
           <View style={styles.HeaderQuestion}>
-            <Text style={styles.headerQuestionText}>What does <Text style={{ fontStyle: 'italic' }}>{nameOfApp}</Text> do then? TL;DR answer, please.</Text>
+            <Text style={styles.headerQuestionText}>What does <Text style={{ fontStyle: 'italic' }}>{nameOfApp}</Text> do then? TL;DR answer.</Text>
           </View>
           <Text style={styles.AnswerText}>This is an app that stores the names + descriptions of the people you meet within your social groups.
           </Text>
@@ -38,7 +38,7 @@ export default class About extends Component {
             <Text style={styles.headerQuestionText}>Can you give an example? </Text>
           </View>
           <Text style={styles.AnswerText}><Text style={{ fontWeight: 'bold' }}> 1. </Text> You volunteer weekly at an after-school program.</Text>
-          <Text style={styles.AnswerText}><Text style={{ fontWeight: 'bold' }}> 2. </Text> You notice a shy kid with red spiky hair sitting in the corner</Text>
+          <Text style={styles.AnswerText}><Text style={{ fontWeight: 'bold' }}> 2. </Text> You notice a shy kid with red spiky hair sitting in the corner.</Text>
           <Text style={styles.AnswerText}><Text style={{ fontWeight: 'bold' }}> 3. </Text> Talking to him, you learn Andrew moved here a couple days ago and enjoys playing chess.</Text>
           <Text style={styles.AnswerText}><Text style={{ fontWeight: 'bold' }}> 4. </Text> A minute after the conversation, you write down Andrew&apos;s name along with the description &ldquo;new guy with red spiky hair who likes chess.&rdquo;</Text>
           <Text style={styles.AnswerText}><Text style={{ fontWeight: 'bold' }}> 5. </Text> Right before you volunteer next week, you realize you forgot the name of that new-red-hair-guy-who-plays-chess.</Text>
